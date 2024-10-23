f1166fbdc7b8d4304c2c0f337e608c1bc92d155c
karlotimmerman <karlo.timmerman@skydust.io>
2024-10-23 09:15:33 +0200

    testprisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import { withOptimize } from '@prisma/extension-optimize';
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
@@ -10,6 +11,6 @@ export const prisma =
     log: process.env.NODE_ENV === 'development' 
       ? ['query', 'error', 'warn'] 
       : ['error'],
-  })
+  }).$extends(withOptimize({ apiKey: process.env.PRISMA_OPTIMIZE_API_KEY ?? '' }));
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
